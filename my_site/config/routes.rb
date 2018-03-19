e36c8de3beb2c3d10b8ae2082ae3a703473b0fa0
victorc <colta.vicu@gmail.com>
2018-03-19 11:01:57 +0200

    add page controllers and stylesheets

--- my_site/config/routes.rb
+++ my_site/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'homepage/index'
+  get 'contact/index'
+  get 'about/index'
+
   root 'homepage#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
